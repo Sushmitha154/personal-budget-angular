3b66b94ffaa20c4b0a9cd456ad7e7bf7b9c75f7e
Sushmitha154 <savula1@uncc.edu>
2024-09-13 12:30:13 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
+// Budget API
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.use('/', express.static('public'));
+
+
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
+app.get('/budget', (req, res) => {
+    const fs = require('fs');
+
+    fs.readFile('budget.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            return;
+        }
+
+        try {
+            const jsonData = JSON.parse(data);
+            res.json(jsonData)
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    });
+
+});
+
+app.get('/getD3Data', (req, res) => {
+    const fs = require('fs');
+
+    fs.readFile('d3jsData.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            return;
+        }
+
+        try {
+            const jsonData = JSON.parse(data);
+            res.json(jsonData)
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
